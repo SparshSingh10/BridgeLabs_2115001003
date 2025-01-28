a2673c9b66af7a2f4f6e91c75c81730453d277ef
SparshSingh10 <sparsh262002@gmail.com>
2025-01-28 14:34:19 +0530

    calculator added

---
+++ calculator/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Calculator
+{
+
+    public static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Enter the first number");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the operation");
+        switch (Console.ReadLine())
+        {
+            case "+":
+                Add(n1, n2);
+                break;
+            case "-":
+                Substract(n1, n2);
+                break;
+            case "*":
+                Multiply(n1, n2);
+                break;
+            case "/":
+                divide(n1, n2);
+                break;
+            default:
+                Console.WriteLine("Invalid operation");
+                break;
+        }
+
+    }
+    public static void Add(int n1, int n2)
+    {
+        Console.WriteLine("The sum is " + (n1 + n2));
+    }
+
+    public static void Substract(int n1, int n2)
+    {
+        Console.WriteLine("The difference is " + (n1 - n2));
+    }
+    public static void Multiply(int n1, int n2)
+    {
+        Console.WriteLine("The product is " + (n1 * n2));
+    }
+    public static void divide(int n1, int n2)
+    {
+        Console.WriteLine("The quotient is " + (n1 / n2));
+    }
+}
